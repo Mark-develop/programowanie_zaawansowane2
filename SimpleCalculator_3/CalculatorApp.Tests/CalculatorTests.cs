00260aa91286331445568291a74cab9bcaedebe7
Mark-develop <markleventsow@gmail.com>
2025-02-02 15:22:13 +0100

    .

--- SimpleCalculator_3/CalculatorApp.Tests/CalculatorTests.cs
+++ SimpleCalculator_3/CalculatorApp.Tests/CalculatorTests.cs
@@ -10,17 +10,13 @@ namespace CalculatorApp.Tests
         [Test]
         public void Add_ShouldSendDataToWebService()
         {
-            // Arrange
             var webServiceMock = new Mock<IWebService>();
             var calculator = new Calculator(webServiceMock.Object);
 
-            // Act
             int result = calculator.Add(3, 5);
 
-            // Assert
             Assert.AreEqual(8, result);
 
-            // Verify that the SendData method was called with the expected argument
             webServiceMock.Verify(ws => ws.SendData("Add operation: 3 + 5 = 8"), Times.Once);
         }
     }
